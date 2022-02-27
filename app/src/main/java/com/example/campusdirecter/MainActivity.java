2c3bd954f901bb4a27ec271d132a23ca025dad66
jureao <s3003946@ba-sachsen.de>
2022-02-27 21:46:52 +0100

    simple get-request to pokeapi. isnt completly connected to ui

--- app/src/main/java/com/example/campusdirecter/MainActivity.java
+++ app/src/main/java/com/example/campusdirecter/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.campusdirecter;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.campusdirecter.api.SingletonRequestQueue;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +22,17 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.campusdirecter.databinding.ActivityMainBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    String pokemon ="";
+    String url = "https://pokeapi.co/api/v2/pokemon/";
+    EditText showPokeName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        getPokeName(1);
+        showPokeName = findViewById(R.id.et_getName);
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -32,6 +53,40 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+
+
     }
+    public void getPokeName(int number)
+    {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url+number, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d("Pokemon: ", response.getString("name"));
+                            pokemon = response.getString("name");
+                            showPokeName.setText(pokemon);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("someting went wrong: ", error.toString());
+
+                    }
+                });
+        SingletonRequestQueue.getInstance(MainActivity.this).addToRequestQueue(jsonObjectRequest);
+
+
+    }
+
+
 
 }
--- app/src/main/java/com/example/campusdirecter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/campusdirecter/ui/home/HomeFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,19 +15,27 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.campusdirecter.MainActivity;
 import com.example.campusdirecter.R;
+import com.example.campusdirecter.api.SingletonRequestQueue;
 import com.example.campusdirecter.databinding.FragmentHomeBinding;
+
+import org.json.JSONObject;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
@@ -43,4 +54,6 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
