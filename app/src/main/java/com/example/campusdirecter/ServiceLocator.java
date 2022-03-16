ec17ebfdd095fd2480c8a507d898c7388f897417
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-16 17:48:48 +0100

    Globalize Gson configuration

--- app/src/main/java/com/example/campusdirecter/ServiceLocator.java
+++ app/src/main/java/com/example/campusdirecter/ServiceLocator.java
@@ -1,5 +1,7 @@
 package com.example.campusdirecter;
 
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
+
 import android.content.Context;
 
 import com.example.campusdirecter.http.HttpClient;
@@ -8,6 +10,19 @@ import com.example.campusdirecter.student.model.StudentRepository;
 import com.example.campusdirecter.student.support.HttpStudentRepository;
 import com.example.campusdirecter.timetable.model.TimetableRepository;
 import com.example.campusdirecter.timetable.support.HttpTimetableRepository;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * @author Vincent Nadoll (s3003870@ba-sachsen.de)
@@ -18,6 +33,7 @@ public class ServiceLocator {
     private static Context context;
 
     private HttpClient client;
+    private GsonBuilder gsonBuilder;
 
     private ServiceLocator(Context context) {
         ServiceLocator.context = context;
@@ -40,6 +56,14 @@ public class ServiceLocator {
         return client;
     }
 
+    public GsonBuilder getGsonBuilder() {
+        if (null == gsonBuilder) {
+            gsonBuilder = new GsonBuilder()
+                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeConverter());
+        }
+        return gsonBuilder;
+    }
+
     public StudentRepository getStudentRepository() {
         HttpClient client = getHttpClient();
         return new HttpStudentRepository(client);
@@ -48,5 +72,20 @@ public class ServiceLocator {
     public TimetableRepository getTimetableRepository() {
         HttpClient client = getHttpClient();
         return new HttpTimetableRepository(client);
+    }
+
+
+    private static class LocalDateTimeConverter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+
+        @Override
+        public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            ZonedDateTime zonedDateTime = ZonedDateTime.from(ISO_DATE_TIME.parse(json.getAsString()));
+            return zonedDateTime.toLocalDateTime();
+        }
+
+        @Override
+        public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(ISO_DATE_TIME.format(src.atZone(ZoneId.systemDefault())));
+        }
     }
 }
