698cf820b28b8a8d7dda022eab968ae48884f538
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-16 17:46:27 +0100

    Make duration parameterizable

--- app/src/main/java/com/example/campusdirecter/model/Interval.java
+++ app/src/main/java/com/example/campusdirecter/model/Interval.java
@@ -1,27 +1,34 @@
 package com.example.campusdirecter.model;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class Interval {
     private LocalDateTime start;
     // duration in minutes
-    private long duration;
+    private String duration;
 
-    public Interval(LocalDateTime start, long duration) {
+    public Interval(LocalDateTime start, String duration) {
         this.start = start;
         this.duration = duration;
+    }
+
+    private Duration parse(String duration) {
+        // TODO: Parse SI unit
+        String decimalValue = duration.replaceAll("/D*", "");
+        return Duration.of(Long.parseLong(decimalValue), ChronoUnit.MINUTES);
     }
 
     public LocalDateTime getStart() {
         return start;
     }
 
-    public long getDuration() {
-        return duration;
+    public Duration getDuration() {
+        return parse(duration);
     }
 
     public LocalDateTime getEnd() {
-        return start.plus(duration, ChronoUnit.MINUTES);
+        return start.plus(parse(duration));
     }
 }
