3162b24bede56971c0ac99aed0a87a769408f58b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-18 16:53:54 +0100

    Fix duration parsing

--- app/src/main/java/com/example/campusdirecter/model/Interval.java
+++ app/src/main/java/com/example/campusdirecter/model/Interval.java
@@ -3,9 +3,25 @@ package com.example.campusdirecter.model;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
+/**
+ * Value Object that represents a time interval. The provided duration must be string compatible
+ * with the defined pattern in {@link SiParser#PATTERN}.
+ *
+ * @author Saboffzl
+ * @author Vincent Nadoll (s3003870@ba-sachsen.de)
+ */
 @Value
 public class Interval {
 
@@ -13,16 +29,77 @@ public class Interval {
     String duration;
 
     public Duration getDuration() {
-        return parse(duration);
+        return SiParser.parse(duration);
     }
 
     public LocalDateTime getEnd() {
-        return start.plus(getDuration());
+        Duration duration = SiParser.parse(this.duration);
+        return start.plus(duration);
     }
 
-    private Duration parse(String duration) {
-        // TODO: Parse SI unit
-        String decimalValue = duration.replaceAll("\\D*", "");
-        return Duration.of(Long.parseLong(decimalValue), ChronoUnit.MINUTES);
+
+    /**
+     * @author Vincent Nadoll (s3003870@ba-sachsen.de)
+     */
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    private enum SiParser {
+        DAYS("d", ChronoUnit.DAYS),
+        HOURS("h", ChronoUnit.HOURS),
+        MINUTES("m", ChronoUnit.MINUTES),
+        SECONDS("s", ChronoUnit.SECONDS),
+        MILLIS("ms", ChronoUnit.MILLIS),
+        ;
+
+        /**
+         * Matches any positive integer w/ at least one digit an optional whitespace and a mandatory
+         * ASCII-alphabetical string w/ at least one character. So it matches:
+         * <ul>
+         *     <li>90m</li>
+         *     <li>0 m</li>
+         *     <li>1337 abc</li>
+         * </ul>
+         * but not
+         * <ul>
+         *     <li>1.5 h</li>
+         *     <li>m</li>
+         *     <li>&nbsp;</li>
+         *     <li>ä</li>
+         *     <li>12</li>
+         * </ul>
+         * Keep in mind that the actual conversion requires the unit, the tail part of the string,
+         * to match one of the SI units defined above. This pattern won't restrict these.
+         */
+        private static final Pattern PATTERN = Pattern.compile("(\\d+)\\s?([a-zA-Z]+)");
+
+        private final String siUnit;
+        private final ChronoUnit unit;
+
+        public static Duration parse(String duration) {
+            Matcher matcher = PATTERN.matcher(duration);
+            matcher.matches();
+            String number = matcher.group(1);
+            String unit = matcher.group(2);
+
+            SiParser parser = from(unit);
+            return Duration.of(Long.parseLong(number), parser.getUnit());
+        }
+
+        private static SiParser from(String siUnit) {
+            return Arrays.stream(values())
+                    .filter(bySiUnit(matches(siUnit)))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException(""
+                            + "Unable to find SI unit parser for arg="
+                            + siUnit));
+        }
+
+        private static Predicate<SiParser> bySiUnit(Function<String, Boolean> predicate) {
+            return siParser -> predicate.apply(siParser.getSiUnit());
+        }
+
+        private static Function<String, Boolean> matches(String obj) {
+            return self -> Objects.equals(self, obj);
+        }
     }
 }
--- app/src/test/java/com/example/campusdirecter/model/IntervalTest.java
+++ app/src/test/java/com/example/campusdirecter/model/IntervalTest.java
@@ -21,11 +21,47 @@ public class IntervalTest {
     }
 
     @Test
+    public void determiningEndDate_shouldInterpretDayDuration() {
+        Interval interval = new Interval(now, "90d");
+
+        LocalDateTime end = interval.getEnd();
+
+        assertEquals(now.plus(90, ChronoUnit.DAYS), end);
+    }
+
+    @Test
+    public void determiningEndDate_shouldInterpretHourDuration() {
+        Interval interval = new Interval(now, "90 h");
+
+        LocalDateTime end = interval.getEnd();
+
+        assertEquals(now.plus(90, ChronoUnit.HOURS), end);
+    }
+
+    @Test
     public void determiningEndDate_shouldInterpretMinuteDuration() {
         Interval interval = new Interval(now, "90m");
 
         LocalDateTime end = interval.getEnd();
 
         assertEquals(now.plus(90, ChronoUnit.MINUTES), end);
+    }
+
+    @Test
+    public void determiningEndDate_shouldInterpretSecondDuration() {
+        Interval interval = new Interval(now, "90 s");
+
+        LocalDateTime end = interval.getEnd();
+
+        assertEquals(now.plus(90, ChronoUnit.SECONDS), end);
+    }
+
+    @Test
+    public void determiningEndDate_shouldInterpretMillisecondDuration() {
+        Interval interval = new Interval(now, "90ms");
+
+        LocalDateTime end = interval.getEnd();
+
+        assertEquals(now.plus(90, ChronoUnit.MILLIS), end);
     }
 }
