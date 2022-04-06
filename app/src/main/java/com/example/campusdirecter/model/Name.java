66d11e97f58ec261767852502b9b235ee4c4bd99
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-04-06 18:56:28 +0200

    Simplify person's full name getter

--- app/src/main/java/com/example/campusdirecter/model/Name.java
+++ app/src/main/java/com/example/campusdirecter/model/Name.java
@@ -1,6 +1,5 @@
 package com.example.campusdirecter.model;
 
-import java.util.StringJoiner;
 import lombok.Value;
 
 @Value
@@ -23,20 +22,6 @@ public class Name {
 
     private String salutation() {
         return null == salutation ? "" : salutation;
-    }
-
-    public String getFullName()
-    {
-        StringJoiner sj = new StringJoiner(" ");
-        if (firstName != null) {
-            sj.add(firstName);
-        }
-        if (lastName != null)
-        {
-            sj.add(lastName);
-        }
-
-        return sj.toString();
     }
 
     public String getInitials()
