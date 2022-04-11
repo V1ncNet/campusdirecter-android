a625f907ca30a37c16fd47d506e5c7df37646ac8
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-04-11 15:02:17 +0200

    Remove arbitrary email constraint

--- app/src/main/java/com/example/campusdirecter/security/ui/LoginViewModel.java
+++ app/src/main/java/com/example/campusdirecter/security/ui/LoginViewModel.java
@@ -54,10 +54,7 @@ public class LoginViewModel extends ViewModel {
         if (username == null) {
             return false;
         }
-        if (username.contains("@")) {
-            return Patterns.EMAIL_ADDRESS.matcher(username).matches();
-        } else {
-            return !username.trim().isEmpty();
-        }
+
+        return !username.trim().isEmpty();
     }
 }
