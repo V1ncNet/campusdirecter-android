002fc5e36ecf6a809141b32494bfc63a3c30251a
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-04-11 14:56:57 +0200

    Remove welcome message

--- app/src/main/java/com/example/campusdirecter/security/ui/LoginActivity.java
+++ app/src/main/java/com/example/campusdirecter/security/ui/LoginActivity.java
@@ -21,7 +21,6 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.campusdirecter.MainActivity;
-import com.example.campusdirecter.R;
 import com.example.campusdirecter.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends FragmentActivity {
@@ -69,9 +68,6 @@ public class LoginActivity extends FragmentActivity {
                 loadingProgressBar.setVisibility(View.GONE);
                 if (loginResult.getError() != null) {
                     showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
                 }
                 setResult(Activity.RESULT_OK);
 
@@ -122,12 +118,6 @@ public class LoginActivity extends FragmentActivity {
                         passwordEditText.getText().toString());
             }
         });
-    }
-
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
